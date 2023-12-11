edfc3c94e9d34f5d21fe84fd64e98a3fb36d8473
denny-svg <pknowledge@gmail.com>
2023-12-11 23:39:47 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const [num1,num2,num3,num4] = [2,31,19,5];
+const arr = [1,1,11,9,18,20,12,2];
+
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()*10)+1;
+
+let mod = num3%num4;
+
+let max = Math.max(...arr);
